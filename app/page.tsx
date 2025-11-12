b52c122afc1da12458d89a207eee3a30c60f8344
shreyashkakad <shreyashkakad03@gmail.com>
2025-11-12 11:47:03 +0530

    add: responsive homePage

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,16 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="flex-col items-center justify-center text-center mx-[30%] mt-[10%] mb-[5%]">
-        <h1 className="text-4xl font-medium">Find books worth remembering.</h1>
-        <p className="font-light text-lg text-white/95">Because screenshots of book covers don’t count.</p>
+      <div className="flex-col items-center justify-center text-center mx-[30%] mt-[10%] mb-[5%] max-[700px]:mx-[5%] max-[700px]:mt-[30%]">
+        <h1 className="text-4xl font-medium max-[700px]:text-xl">Find books worth remembering.</h1>
+        <p className="font-light text-lg text-white/95 max-[700px]:text-sm">Because screenshots of book covers don’t count.</p>
       </div>
-      <div className="mx-[30%] items-center text-center text-xl">
-        <p className="font-regular text-white/90">Search books and add them to your reading list. <br />
+      <div className="mx-[30%] items-center text-center text-xl max-[700px]:mx-[5%] max-[700px]:mt-[10%]">
+        <p className="font-regular text-white/90 max-[700px]:font-medium max-[700px]:text-sm">Search books and add them to your reading list. <br />
           Check reviews, read summaries, and track what you love.</p>
       </div>
 
-      <div className="mx-[32%] mt-[4%] flex justify-between">
+      <div className="mx-[32%] mt-[4%] flex justify-between max-[700px]:mx-[15%] max-[700px]:mt-[12%] max-[700px]:flex-col max-[700px]:gap-3">
         <div className="p-3 flex items-center rounded-xl border border-white/20">
           <span className="inline-flex items-center justify-center bg-purple-900/15 p-3 rounded-xl">
             <Image src="/plus-solid.svg" alt="plusIcon" width={16} height={16} className="block" />
@@ -38,7 +38,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="mx-[30%] mt-8 flex justify-center">
+      <div className="mx-[30%] mt-8 flex justify-center max-[700px]:mx-[15%]">
         <button
           className="bg-white py-2 px-4 flex items-center gap-2 text-black rounded-md shadow cursor-pointer transform transition-transform duration-150 ease-out active:scale-95"
         >
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,26 +1,40 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => setIsMobile(window.innerWidth < 700);
+        handleResize();
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
     return (
-        <nav className="flex justify-between items-center mx-[15%] my-[1%]">
+        <nav className="flex justify-between items-center mx-[15%] my-[1%] max-[700px]:mx-[3%]">
             <Link href="/" className="flex items-center gap-3">
                 <Image src="/bars-staggered-solid.svg" alt="User" width={20} height={20} className="h-5 w-5" />
-                <span className="text-2xl font-semibold">BookShelf</span>
+                <span className="text-2xl font-semibold max-[700px]:text-xl">BookShelf</span>
             </Link>
 
             <div className="flex justify-center items-center">
-                <div className="bg-black py-2 rounded-2xl px-6 outline-none flex items-center">
-                    <input type="text" placeholder="Search Books" className="outline-none font-semibold text-white/60" />
-                    <button className="cursor-pointer">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-arrow-right h-3 w-3" aria-hidden="true">
-                            <path d="M5 12h14" />
-                            <path d="m12 5 7 7-7 7" />
-                        </svg>
-                    </button>
+                <div className="bg-black py-2 rounded-2xl px-4 outline-none flex items-center max-[700px]:px-2 max-[700px]:py-1.5">
+                    <input
+                      type="text"
+                      placeholder={isMobile ? "Search" : "Search Books"}
+                      className="outline-none font-semibold text-white/60 w-50 max-[700px]:w-22"
+                    />
+                    <Link href="/user" className="cursor-pointer">
+                        <Image src="/arrow-right-solid.svg" alt="" width={20} height={20} className="h-4 w-4"/>
+                    </Link>
                 </div>
 
-                <Link href="/user" className="ml-5">
+                <Link href="/user" className="ml-5 max-[700px]:ml-2">
                     <Image src="/user-solid.svg" alt="User" width={20} height={20} className="filter brightness-0 invert h-4 w-4" />
                 </Link>
             </div>
